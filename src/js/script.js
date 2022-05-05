137defeb70758f062217a17fe77fb525cc2f1d73
Denys Protsenko <finzigerald953@gmail.com>
2022-05-05 17:29:27 +0300

    src

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,17 @@
-
-
-
+/* let login = prompt('');
+let password
+if (login == 'Admin'){
+    password = prompt('Your password?')
+} else if (login == '' || login == null){
+    alert('Exit')
+} else if (login !==(login == '' || login == null)) {
+    alert('I do not know who are you');
+}    
+if (password == 'i am a boss'){
+    alert('Здравствуйте')
+} else if (password == '' || password == null){
+    alert('Exit')
+} else if (password !== (password == '' || password == null)){
+    alert('Blocked')
+}
+ */
